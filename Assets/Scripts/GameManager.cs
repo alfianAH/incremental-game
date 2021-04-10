6418f8e9bd9285b502866aeb63b0e1a2172f9b8d
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-04-10 21:35:28 +0800

    Make scripts to change Resource UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public struct ResourceConfig
+{
+    public string name;
+    public double unlockCost;
+    public double upgradeCost;
+    public double output;
+}
---
+++ Assets/Scripts/ResourceControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceControl : MonoBehaviour
+{
+    [SerializeField] private Text resourceDescription,
+        resourceUpgradeCost,
+        resourceUnlockCost;
+
+    private ResourceConfig config;
+
+    private int level = 1;
+
+    public void SetConfig(ResourceConfig configuration)
+    {
+        this.config = configuration;
+
+        resourceDescription.text = $"{config.name} Lv. {level}\n+{GetOutput():0}";
+        resourceUnlockCost.text = $"Unlock Cost\n{config.unlockCost}";
+        resourceUpgradeCost.text = $"Upgrade Cost\n{GetUpgradeCost()}";
+    }
+
+    public double GetOutput()
+    {
+        return config.output * level;
+    }
+
+    public double GetUpgradeCost()
+    {
+        return config.upgradeCost * level;
+    }
+
+    public double GetUnlockCost()
+    {
+        return config.unlockCost;
+    }
+}
