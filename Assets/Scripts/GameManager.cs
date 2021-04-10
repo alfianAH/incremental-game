7c84086147e2d2e5d3e3a9e883df20d255591d6e
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-04-10 22:05:36 +0800

    Make gold auto increase

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,83 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager instance;
+
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<GameManager>();
+
+            return instance;
+        }
+    }
+
+    [Range(0, 1)] public float autoCollectPercentage = 0.1f;
+    public ResourceConfig[] resourceConfigs;
+
+    public Transform resourceParent;
+    public ResourceControl resourcePrefab;
+
+    [SerializeField] private Text goldInfo,
+        autoCollectInfo;
+    
+    private List<ResourceControl> activeResources = new List<ResourceControl>();
+    private float collectSecond;
+    private double totalGold;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        AddAllResources();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        collectSecond += Time.unscaledDeltaTime;
+
+        if (collectSecond >= 1f)
+        {
+            CollectPerSecond();
+            collectSecond = 0f;
+        }
+    }
+
+    private void AddAllResources()
+    {
+        foreach (ResourceConfig config in resourceConfigs)
+        {
+            GameObject obj = Instantiate(resourcePrefab.gameObject, resourceParent, false);
+            ResourceControl resource = obj.GetComponent<ResourceControl>();
+            
+            resource.SetConfig(config);
+            activeResources.Add(resource);
+        }
+    }
+
+    private void CollectPerSecond()
+    {
+        double output = 0;
+        foreach (ResourceControl resource in activeResources)
+        {
+            output += resource.GetOutput();
+        }
+
+        output *= autoCollectPercentage;
+        autoCollectInfo.text = $"Auto Collect: {output:F1} / second";
+
+        AddGold(output);
+    }
+
+    private void AddGold(double value)
+    {
+        totalGold += value;
+        goldInfo.text = $"Gold: {totalGold:0}";
     }
 }
 
