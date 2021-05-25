155c4cbb93228c1ef14f098f72d9a9052b639ec3
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-05-25 11:23:35 +0800

    Make loading scene

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        UserDataManager.Load();
+        SceneManager.LoadScene(1);
+    }
+}
